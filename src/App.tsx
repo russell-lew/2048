70313b00978a78b51ab8132b863849e8386e4683
Russell Lew <russell.lyf@gmail.com>
2026-05-09 15:59:18 +0800

    Apply colors and theme

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import { Board } from './components/Board'
 function App() {
   return (
     <div className="flex h-screen w-full flex-col bg-gray-100">
-      <header className="flex-none bg-white p-4 shadow-md">
-        <h1 className="text-xl font-bold text-gray-800">2048</h1>
+      <header className="flex-none bg-background-10 p-4">
+        <h1 className="text-xl text-center font-bold text-primary-text-color">
+          2048
+        </h1>
       </header>
 
-      <main className="flex flex-1 items-center justify-center overflow-hidden p-4">
+      <main className="flex flex-1 items-center justify-center overflow-hidden p-4 bg-background-10">
         <Board />
       </main>
     </div>
