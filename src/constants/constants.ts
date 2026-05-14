a47f6287ddd4c41bf6ad0c5937ab9ffe32bd1443
Russell Lew <russell.lyf@gmail.com>
2026-05-14 09:31:09 +0800

    Adjust max new tiles configuration to 1

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,7 +1,7 @@
 export const BOARD_SIZE = 4
 export const WIN_CONDITION = 2048
 export const SPAWN_RATIO_2 = 0.8
-export const MAX_NEW_TILES = 5
+export const MAX_NEW_TILES = 1
 
 export const CTA_LABEL_DEFAULT = 'Analyze best move'
 export const CTA_LABEL_LOADING = 'Thinking...'
